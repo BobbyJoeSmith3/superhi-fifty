f7e37fd596275afe22014527109ec0d554e518e8
Bobby Joe Smith III <bobbyjoesmithiii@MacBook-Pro-145.lan>
2024-08-17 23:08:01 -0700

    first commit create structure for entire page and style the intro section animate marquee

---
+++ fifty.js
@@ -0,0 +1,16 @@
+function makeMarquee () {
+    const title = 'FIFTY Music Festival—November 10-12, Desert Valley';
+
+    // an array constructor which creates a new empty array with 50 slots
+    // the fill method will populate the array with specified items
+    // join method joins together all of the elements in the array into one text string with a specified delineator
+    const marqueeText = new Array(50).fill(title).join(' — ');
+
+    // 1. grab our marquee span from the html
+    // 2. we want to set our repeating title as the content
+    const marquee = document.querySelector('.marquee span');
+    marquee.innerHTML = marqueeText;
+
+}
+
+makeMarquee();
